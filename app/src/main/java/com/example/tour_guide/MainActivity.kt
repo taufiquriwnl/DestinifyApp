cc3fa7eadcf9ad3c7630ac7b9a9d117e321558a5
unknown <fidargamer123@gmail.com>
2023-12-21 19:51:49 +0700

    Final Massive

---
+++ app/src/main/java/com/example/tour_guide/MainActivity.kt
@@ -0,0 +1,60 @@
+package com.example.tour_guide
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val shopingview = findViewById<ImageView>(R.id.shopping_cart_icon)
+        shopingview.setOnClickListener {
+            val intent = Intent(this, ShoppingCartActivity::class.java)
+            startActivity(intent)
+        }
+
+
+        val buttonGuider1 = findViewById<Button>(R.id.buttonguider1)
+        buttonGuider1.setOnClickListener {
+            val intent = Intent(this, DetailGuider1Activity::class.java)
+            startActivity(intent)
+        }
+
+        val buttonGuider2 = findViewById<Button>(R.id.buttonguider2)
+        buttonGuider2.setOnClickListener {
+            val intent = Intent(this, DetailGuider2Activity::class.java)
+            startActivity(intent)
+        }
+
+        val destinasiImageView = findViewById<ImageView>(R.id.destinasi)
+        destinasiImageView.setOnClickListener {
+            val intent = Intent(this, DetailDestinasiActivity::class.java)
+            startActivity(intent)
+        }
+
+        val btntourguide = findViewById<Button>(R.id.btntourguide)
+
+        btntourguide.setOnClickListener{
+            Intent(this, TourGuideActivity::class.java).also {
+                startActivity(it)
+            }
+        }
+
+        val btndestinasi = findViewById<Button>(R.id.btndestinasi)
+
+        btndestinasi.setOnClickListener{
+            Intent(this, DestinasiActivity::class.java).also {
+                startActivity(it)
+            }
+        }
+
+    }
+}
---
+++ app/src/main/java/com/example/tour_guide/TourGuideActivity.kt
@@ -0,0 +1,11 @@
+package com.example.tour_guide
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TourGuideActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tour_guide)
+    }
+}
